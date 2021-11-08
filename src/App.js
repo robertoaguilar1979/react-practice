28395d904f56a138e5aa0fa963e1136dea65a1aa
robertoaguilar1979 <robertoaguilar1979@gmail.com>
2021-11-07 21:37:48 -0800

    added new map method

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import { useState } from "react";
 
 function App() {
   let [name, setName] = useState("mario");
-
+  const [title, setTitle] = useState([
+    { title: "hello world", id: 1 },
+    { title: "hello planet", id: 2 },
+    { title: "hello universe", id: 3 },
+  ]);
   const handleClick = () => {
     setName("luigi");
     console.log(name);
@@ -13,6 +17,13 @@ function App() {
     <div className="App">
       <h1>Hello {name}</h1>
       <button onClick={handleClick}>click me</button>
+      {title.map((title, index) => (
+        <div key={title.id}>
+          <h2>
+            {index} - {title.title}
+          </h2>
+        </div>
+      ))}
     </div>
   );
 }
